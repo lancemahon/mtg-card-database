a2be2efc7107312204991c6f7589d691de2feee1
Lance Mahon <lance.mahon@gmail.com>
2019-10-16 12:35:00 -0400

    Using some card routes now

---
+++ app/models/deck.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const deckSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  colors: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  format: {
+    type: String
+  },
+  commander: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Card'
+  },
+  legal: {
+    type: Boolean
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Deck', deckSchema)
