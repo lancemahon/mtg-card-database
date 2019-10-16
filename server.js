2f90b23e2659a650367381565758b85dfa935069
Lance Mahon <lance.mahon@gmail.com>
2019-10-16 12:35:55 -0400

    Put the card routes into server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const deckRoutes = require('./app/routes/deck_routes')
+const cardRoutes = require('./app/routes/card_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -67,6 +68,7 @@ app.use(requestLogger)
 app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(deckRoutes)
+app.use(cardRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
